09b61e715c8ba67244921d52e93edc42dee7c5fe
Tomas Sedlacek <digitalaxon@gmail.com>
2020-04-27 10:44:03 +0200

    Cleaned some code

--- program.py
+++ program.py
@@ -3,12 +3,13 @@ import pygame.freetype
 import time
 import random
 
-#Initialization of variables
+#Initialization of global variables
 size = width, height = 320,240
-black = 0, 0, 0
+blue = 0, 0, 255
 white = 255, 255, 255
 y = 100
 z = 0
+word = "Test"
 
 #Initialization
 screen = pygame.display.set_mode(size)
@@ -23,16 +24,23 @@ def word_render(lines,x,y):
     text_colour = random_colour()
 
     #Screen fils black, then font is rendered on screen
-    screen.fill(black)
     pygame.freetype.font.render_to(screen, dest, lines, text_colour)
+    pygame.display.update()
 
 #Prints character one by one
 def character_render(z):
-    word = "LoremIpsum"
     text = word[z]
     dest = (150,150)
+
     pygame.freetype.font.render_to(screen, dest, text, white)
     pygame.display.update()
+
+def render(word,x,y,z):
+    screen.fill(blue)
+    #Function call to render moving word
+    word_render(word,x,y)
+    #Function call to render single character
+    z = character_render(z)
 
 #Changes the colour of text randomly
 def random_colour():
@@ -44,21 +52,16 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
-    screen.fill(black)
-
-    #function call for rendering text at x
-    for x in range(0,240,5):
-        word_render("Sedlor",x,y)
+    for x in range(0,width,5):
         z += 1
-        if (z > 9):
+        if (z > len(word)-1):
             z = 0
-        character_render(z)
+        render(word,x,y,z)
 
-    for x in range(240,0,-5):
-        word_render("Sedlor",x,y)
+    for x in range(width,0,-5):
         z += 1
-        if (z > 9):
+        if (z > len(word)-1):
             z = 0
-        character_render(z)
+        render(word,x,y,z)
 
     pygame.display.flip()
